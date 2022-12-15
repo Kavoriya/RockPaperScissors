21dc4e36f02ce68302e84b65a802ee0b13a37f21
Roman <kavoriya@gmail.com>
2022-12-15 19:20:54 +0500

    Added new features to ui
     - "Result circles" change colors according to the outcome
      of a round. They also turn grey when new game starts.
     - Added "game over" messages.
     - "Try again" button now only appears when the game is over.
     - Shape button now get disabled when game is over, and
      get enabled when user presses "try again" button.

--- newScript.js
+++ newScript.js
@@ -3,86 +3,129 @@ let computerShape = "";
 let playerScore = 0;
 let computerScore = 0;
 let numberOfRounds = 0;
+let outcome = "";
 
 let rockButton = document.getElementById("shape-rock");
 let paperButton = document.getElementById("shape-paper");
 let scissorsButton = document.getElementById("shape-scissors")
 
+let titleText = document.getElementById("title");
+let tryAgainButton = document.getElementById("tryAgain");
+
+let results = document.getElementsByClassName("result");
+
 rockButton.addEventListener("click", playRoundRock);
 paperButton.addEventListener("click", playRoundPaper);
 scissorsButton.addEventListener("click", playRoundScissors);
-
-
+tryAgainButton.addEventListener("click", resetGame);
 
 function checkResults() {
   if (numberOfRounds == 5) {
+  disableShapeButtons();
     if (playerScore > computerScore) {
-      console.log("You won!");
-    } else if ( computerScore > playerScore) {
-      console.log("You lose!");
+      titleText.textContent = "You won!";
+    } else if (computerScore > playerScore) {
+      titleText.textContent = "You lose!";
     } else {
-      console.log("Draw!");
+      titleText.textContent = "Draw!";
     }
-    numberOfRounds = 0;
-    playerScore = 0;
-    computerScore = 0;
+    tryAgainButton.style.display = "block";
   }
 }
 
+function resetGame() {
+  enableShapeButtons();
+  for (let i = 0; i < results.length; i++) {
+    results.item(i).style.backgroundColor = "grey";
+  }
+  numberOfRounds = 0;
+  playerScore = 0;
+  computerScore = 0;
+  tryAgainButton.style.display = "none";
+  titleText.textContent = "Choose your shape";
+}
+
+function changeColor() {
+  let result = document.getElementById(`result-${numberOfRounds}`);
+  if (outcome == "win") {
+    result.style.backgroundColor = "green";
+  }
+  if (outcome == "lose") {
+    result.style.backgroundColor = "red";
+  }
+  if (outcome == "draw") {
+    result.style.backgroundColor = "yellow";
+  }
+}
 
 function playRoundRock() {
   computerShape = getComputerShape();
   numberOfRounds++;
   if (computerShape === "Rock") {
-    console.log("draw");
+    outcome = "draw";
   }
   if (computerShape === "Paper") {
-    console.log("lose");
+    outcome = "lose";
     computerScore++;
   }
   if (computerShape === "Scissors") {
-    console.log("win");
+    outcome = "win";
     playerScore++;
   }
   checkResults();
+  changeColor();
 }
 
 function playRoundPaper() {
   computerShape = getComputerShape();
   numberOfRounds++;
   if (computerShape === "Rock") {
-    console.log("win");
+    outcome = "win";
     playerScore++;
   }
   if (computerShape === "Paper") {
-    console.log("draw");
+    outcome = "draw";
   }
   if (computerShape === "Scissors") {
-    console.log("lose");
+    outcome = "lose";
     computerScore++;
   }
   checkResults();
+  changeColor();
 }
 
 function playRoundScissors() {
   computerShape = getComputerShape();
   numberOfRounds++;
   if (computerShape === "Rock") {
-    console.log("lose");
+    outcome = "lose";
     computerScore++;
   }
   if (computerShape === "Paper") {
-    console.log("win");
+    outcome = "win";
     playerScore++;
   }
   if (computerShape === "Scissors") {
-    console.log("draw");
+    outcome = "draw";
   }
   checkResults();
+  changeColor();
 }
   
 function getComputerShape() {
   let choices = ["Rock", "Paper", "Scissors"];
   let i = Math.floor(Math.random() * 3);
   return choices[i];
+}
+
+function disableShapeButtons() {
+  rockButton.disabled = true;
+  paperButton.disabled = true;
+  scissorsButton.disabled = true;
+}
+
+function enableShapeButtons() {
+  rockButton.disabled = false;
+  paperButton.disabled = false;
+  scissorsButton.disabled = false;
 }
